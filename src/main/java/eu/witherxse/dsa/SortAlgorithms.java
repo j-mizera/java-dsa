d6fefcacee0a3f4a6095f7e7aab662734cb1b45e
witherxse <k.cstmz@gmail.com>
2024-08-11 12:16:39 +0200

    Add quicksort and simple github workflow that builds projects and runs tests

--- src/main/java/eu/witherxse/dsa/SortAlgorithms.java
+++ src/main/java/eu/witherxse/dsa/SortAlgorithms.java
@@ -2,6 +2,7 @@ package eu.witherxse.dsa;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 public class SortAlgorithms {
 
@@ -10,6 +11,7 @@ public class SortAlgorithms {
      * @param arr
      */
     public static void bubbleSort(int[] arr) {
+        Objects.requireNonNull(arr);
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -21,8 +23,40 @@ public class SortAlgorithms {
         }
     }
 
+    public static void quickSort(int[] arr) {
+        Objects.requireNonNull(arr);
+        quickSortInternal(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSortInternal(int[] arr, int low, int high) {
+        if (low < high) {
+            final int pivotIdx = quickSortPartition(arr, low, high);
+            quickSortInternal(arr, low, pivotIdx - 1);
+            quickSortInternal(arr, pivotIdx + 1, high);
+        }
+    }
+
+    private static int quickSortPartition(int[] arr, int low, int high) {
+        final int pivot = arr[high];
+        int idx = low - 1;
+
+        for (int i = low; i < high; i++) {
+            if (arr[i] <= pivot) {
+                idx++;
+                final int tmp = arr[i];
+                arr[i] = arr[idx];
+                arr[idx] = tmp;
+            }
+        }
+
+        idx++;
+        arr[high] = arr[idx];
+        arr[idx] = pivot;
+        return idx;
+    }
+
     public static Collection<SortAlgorithm> allAlgorithms() {
-        return List.of(SortAlgorithms::bubbleSort);
+        return List.of(SortAlgorithms::bubbleSort, SortAlgorithms::quickSort);
     }
 
     @FunctionalInterface
